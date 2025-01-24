f9492777b407dc6c4de32253cffbfdb10617f35b
Anubhav Gupta <anubhav.gupta@fortanix.com>
2025-01-24 11:04:01 +0530

    fix linting

--- app.py
+++ app.py
@@ -7,22 +7,26 @@ app = Flask(__name__)
 # Load the trained Fine-tuned Naive Bayes model
 model = joblib.load('model/tuned_naive_bayes_model.pkl')
 
+
 @app.route('/')
 def home():
     return "Model is up and running!"
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()  # Expecting a JSON payload with input data
-    
+
     # Get input data
-    input_data = np.array(data['input']).reshape(1, -1)  # Assuming input is a list of features
-    
+    input_data = np.array(data['input']).reshape(
+        1, -1)  # Assuming input is a list of features
+
     # Predict using the model
     prediction = model.predict(input_data)
-    
+
     # Return the result as a JSON
     return jsonify({'prediction': prediction.tolist()})
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
--- test_weather_model.py
+++ test_weather_model.py
@@ -1,38 +1,44 @@
-import pytest
 import pandas as pd
 from train import (
     load_data,
     check_missing_values,
     convert_date_column,
-    label_encoding,
     preprocess_data,
     logistic_regression,
     evaluate_model
 )
 
 # Test for Data Loading
+
+
 def test_load_data():
-    data = load_data("./data/weather.csv")
+    data = load_data("weather.csv")
     assert isinstance(data, pd.DataFrame), "Data is not loaded as a DataFrame"
     assert not data.empty, "Dataframe is empty"
 
 # Test for Missing Values
+
+
 def test_check_missing_values():
     data = load_data("weather.csv")
     missing_values = check_missing_values(data)
     assert missing_values.sum() == 0, "There are missing values in the dataset"
 
 # Test for Date Conversion
+
+
 def test_convert_date_column():
     data = load_data("weather.csv")
     convert_date_column(data)
-    assert pd.api.types.is_datetime64_any_dtype(data['date']), "Date column was not converted to datetime"
+    assert pd.api.types.is_datetime64_any_dtype(
+        data['date']), "Date column was not converted to datetime"
 
 # Test for Logistic Regression Model Accuracy
+
+
 def test_logistic_regression_accuracy():
     data = load_data("weather.csv")
     X_train, X_test, y_train, y_test = preprocess_data(data)
     classifier = logistic_regression(X_train, y_train)
     accuracy = evaluate_model(classifier, X_test, y_test)
-    
-    assert accuracy > 0.5, f"Accuracy for Logistic Regression is too low: {accuracy}"
+    assert accuracy > 0.5, f"Acuracy of Logistic Regression is low: {accuracy}"
